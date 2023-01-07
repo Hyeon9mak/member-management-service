9caaae43f58225d09bcfefcdd0287cc41d456e64
hyeon9mak <jinha3507@gmail.com>
2023-01-07 12:25:53 +0900

    refactor: 사용되지 않는 테스트 관련 빈 제거

--- src/test/kotlin/hyeon9mak/membermanagementservice/MemberManagementServiceApplicationTests.kt
+++ src/test/kotlin/hyeon9mak/membermanagementservice/MemberManagementServiceApplicationTests.kt
@@ -1,13 +1,8 @@
 package hyeon9mak.membermanagementservice
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class MemberManagementServiceApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
 
 }
