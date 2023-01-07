7a5617fceff18d332d071d340f61280e3c6aa540
Hyeon9mak <jinha3507@gmail.com>
2023-01-07 20:40:52 +0900

    feat: 인증번호 인증로직 구현

--- src/main/kotlin/hyeon9mak/membermanagementservice/domain/MemberAuthenticationCode.kt
+++ src/main/kotlin/hyeon9mak/membermanagementservice/domain/MemberAuthenticationCode.kt
@@ -4,13 +4,19 @@ import java.time.LocalDateTime
 import java.util.*
 
 class MemberAuthenticationCode(
-    val phoneNumber: MemberPhoneNumber
+    val id: Long = 0,
+    val phoneNumber: MemberPhoneNumber,
+    val code: String = UUID.randomUUID().toString().take(8),
+    var authenticated: Boolean = false,
+    val createdAt: LocalDateTime = LocalDateTime.now()
 ) {
-    val id: Long = 0
-    val code: String = UUID.randomUUID().toString().take(8)
-    var authenticated: Boolean = false
-    private val createdAt: LocalDateTime = LocalDateTime.now()
     private val expiredAt: LocalDateTime = createdAt.plusMinutes(10L)
+
+    fun authenticate(code: String) {
+        require(this.code == code) { "인증 코드가 일치하지 않습니다." }
+        check(expiredAt >= LocalDateTime.now()) { "인증 코드가 만료되었습니다." }
+        authenticated = true
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
---
+++ src/test/kotlin/hyeon9mak/membermanagementservice/domain/MemberAuthenticationCodeTest.kt
@@ -0,0 +1,36 @@
+package hyeon9mak.membermanagementservice.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.time.LocalDateTime
+
+internal class MemberAuthenticationCodeTest : StringSpec({
+
+    "코드를 인증한다" {
+        val authenticationCode = MemberAuthenticationCode(
+            phoneNumber = MemberPhoneNumber("01012345678"),
+            code = "ABCDEFGH",
+        )
+        authenticationCode.authenticate("ABCDEFGH")
+        authenticationCode.authenticated shouldBe true
+    }
+
+    "코드가 일치하지 않으면 인증에 실패한다." {
+        val authenticationCode = MemberAuthenticationCode(
+            phoneNumber = MemberPhoneNumber("01012345678"),
+            code = "ABCDEFGH",
+        )
+        val exception = shouldThrow<IllegalArgumentException> { authenticationCode.authenticate("ZYXWVUTS") }
+        exception.message shouldBe "인증 코드가 일치하지 않습니다."
+    }
+
+    "인증시간이 10분이 지나면 인증할 수 없다" {
+        val authenticationCode = MemberAuthenticationCode(
+            phoneNumber = MemberPhoneNumber("01012345678"),
+            code = "ABCDEFGH",
+            createdAt = LocalDateTime.now().minusMinutes(30L))
+        val exception = shouldThrow<IllegalStateException> { authenticationCode.authenticate("ABCDEFGH") }
+        exception.message shouldBe "인증 코드가 만료되었습니다."
+    }
+})
