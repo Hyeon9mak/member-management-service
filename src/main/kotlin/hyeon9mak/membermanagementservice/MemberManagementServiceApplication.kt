64f4eaae8abea29aea5da9ed43e117c5961ddf0a
hyeon9mak <jinha3507@gmail.com>
2023-01-07 10:28:45 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/kotlin/hyeon9mak/membermanagementservice/MemberManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package hyeon9mak.membermanagementservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MemberManagementServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MemberManagementServiceApplication>(*args)
+}
---
+++ src/test/kotlin/hyeon9mak/membermanagementservice/MemberManagementServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hyeon9mak.membermanagementservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemberManagementServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
