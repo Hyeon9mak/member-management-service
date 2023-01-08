0acd612f152859d28c127effd7bd07efd6d3b9eb
hyeon9mak <jinha3507@gmail.com>
2023-01-08 17:44:37 +0900

    refactor: 토큰 파싱 실패시 알림 수정

--- src/main/kotlin/hyeon9mak/membermanagementservice/application/login/JwtTokenProvider.kt
+++ src/main/kotlin/hyeon9mak/membermanagementservice/application/login/JwtTokenProvider.kt
@@ -5,6 +5,7 @@ import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
 import org.springframework.stereotype.Component
 import java.util.*
+import javax.naming.AuthenticationException
 
 @Component
 class JwtTokenProvider {
@@ -22,12 +23,18 @@ class JwtTokenProvider {
 
     private fun createExpireDate(now: Date): Date = Date(now.time + ONE_HOUR_IN_MILLISECONDS)
 
-    fun parseToken(token: String): String = Jwts.parserBuilder()
-        .setSigningKey(SECRET_KEY.encoded)
-        .build()
-        .parseClaimsJws(token)
-        .body
-        .subject
+    fun parseToken(token: String): String {
+        try {
+            return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.encoded)
+                .build()
+                .parseClaimsJws(token)
+                .body
+                .subject
+        } catch (e: Exception) {
+            throw AuthenticationException("올바른 인증 정보가 아닙니다.")
+        }
+    }
 
     companion object {
         private const val ONE_HOUR_IN_MILLISECONDS: Long = 1000 * 60 * 60
