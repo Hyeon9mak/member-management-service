b56793c98e84cbecc8e1075870e10ea326f667b2
hyeon9mak <jinha3507@gmail.com>
2023-01-08 14:55:35 +0900

    chore: http request body 변환을 위한 jackson 모듈 의존 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ allOpen {
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
